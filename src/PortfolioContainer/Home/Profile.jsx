fc4af9abe839a43a7db5a3933f7fbe18eed752ba
erodit <em44433@ubt-uni.net>
2022-03-17 18:40:18 +0100

    Adding-Download-CV-Button

--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -1,3 +1,4 @@
+import Typical from "react-typical";
 import React from "react";
 
 export default function Profile() {
@@ -20,6 +21,51 @@ export default function Profile() {
             </a>
             <a href="#">
               <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}Hello, I'm <span className="highlighted-text">Erodit</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {""}
+              <h1>
+                {""}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Dev",
+                    2000,
+                    "Front End Developer",
+                    2000,
+                    "Mern Stack Dev",
+                    2000,
+                    "Cross Platform Dev",
+                    2000,
+                    "React/Native Dev",
+                    2000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of Building applications with front and back end
+                operatinons
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {" "}
+              Hire me
+              {""}
+            </button>
+            <a
+              href="EroditMehmeti-CV.pdf"
+              download="Erodit EroditMehmeti-CV.pdf"
+            >
+              <button className="btn highlighted-btn">Get Resumes</button>
             </a>
           </div>
         </div>
